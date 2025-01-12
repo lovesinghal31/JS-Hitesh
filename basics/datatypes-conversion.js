756416a32b7b98d432ed9e7265923ecd2b3b0c77
Love Singhal <groothoon31@gmail.com>
2025-01-13 00:24:20 +0530

    datatypes and coversion

---
+++ basics/datatypes-conversion.js
@@ -0,0 +1,13 @@
+let score = "23rewr";
+// console.log(score); // print the variable declared as "score"
+// console.log(typeof score); // print the type of variable declared as "score"
+let scoreInNumber = Number(score); // Number used to convert the variable to number
+console.log(scoreInNumber); 
+console.log(typeof scoreInNumber);
+/*
+"23" => 23;
+"23abc" => NaN;
+true => 1;
+false => 0;
+null => 0;
+*/
---
+++ basics/datatypes.js
@@ -0,0 +1,6 @@
+let userName = "Love Singhal";
+let age = 18;
+let isStudent = true;
+console.log(userName);
+console.log(age);
+console.log(isStudent);
