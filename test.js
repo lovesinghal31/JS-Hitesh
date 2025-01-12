0463229c91e0451a82737af255f935e67cf0bd4d
Love Singhal <groothoon31@gmail.com>
2025-01-12 23:30:50 +0530

    Created a simple JS average function to test system functionality

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log("Love Singhal");
+// console.log("Love Singhal");
+let array = [12,32,34,99];
+let sum = 0;
+for(let val of array){
+    sum += val;
+}
+let avg = sum/array.length;
+console.log(avg);
