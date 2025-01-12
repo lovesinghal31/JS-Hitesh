a9a7137afea2c6bb975b1840e46a361d2a44c05f
Love Singhal <groothoon31@gmail.com>
2025-01-13 00:10:15 +0530

    variables

---
+++ basics/variables.js
@@ -0,0 +1,6 @@
+const userId = "2348972rf240239ofhd98283";
+let userNmae = "Love Singhal";
+var userEmail = "love@email.com";
+userPassword = "3423dhfsd393krKLH239@#";
+// console.log(userId,userNmae,userEmail,userPassword);
+console.table([userId,userNmae,userEmail,userPassword]);
